9738f40a06421b975a7dc380792582a56e775ab5
Hamza Khalid <hamza.kha@icloud.com>
2021-12-28 23:04:40 -0800

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Container Deployed On Git PUSH Request From Developer');
+  res.send('**Container Deployed On Git PUSH Request From Developer**');
 });
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
